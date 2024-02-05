67b659ad80f5906b0601868f62f41353cbd848f6
Jaakko Moisio <jaakko@moisio.fi>
2024-02-05 23:09:14 +0200

    Release version 0.8.1

--- pyupgw/__init__.py
+++ pyupgw/__init__.py
@@ -1,6 +1,6 @@
 """Python client for Unisenza Plus"""
 
-__version__ = "0.8"
+__version__ = "0.8.1"
 
 from .client import Client, create_api, create_client
 from .errors import AuthenticationError, ClientError
